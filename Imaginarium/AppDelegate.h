5fb60890b02dd0d46df57eb4289f34ee40de55c8
Scott Sanders <scott@thesandersfam.com>
2015-03-01 20:00:25 -0600

    Initial Commit

---
+++ Imaginarium/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Imaginarium
+//
+//  Created by Scott Sanders on 3/1/15.
+//  Copyright (c) 2015 Scott Sanders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Imaginarium/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Imaginarium
+//
+//  Created by Scott Sanders on 3/1/15.
+//  Copyright (c) 2015 Scott Sanders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
