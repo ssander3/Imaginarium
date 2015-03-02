dabbff387a22413e5534aa2d6077ee931acceee9
Scott Sanders <scott@thesandersfam.com>
2015-03-01 22:47:21 -0600

    End of lecture #10

---
+++ Imaginarium/ImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageViewController.h
+//  Imaginarium
+//
+//  Created by Scott Sanders on 3/1/15.
+//  Copyright (c) 2015 Scott Sanders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewController : UIViewController
+@property (nonatomic, strong) NSURL *imageURL;
+@end
