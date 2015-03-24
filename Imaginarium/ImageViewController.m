3cb0331a6d2c0d6a01db0aa33b562009c0840693
Scott Sanders <scott@thesandersfam.com>
2015-03-23 20:42:12 -0500

    Enhancements from Lecture 11

--- Imaginarium/ImageViewController.m
+++ Imaginarium/ImageViewController.m
@@ -74,8 +74,11 @@
 
 - (void)setImage:(UIImage *)image
 {
+    self.scrollView.zoomScale = 1.0;
     self.imageView.image = image;
-    [self.imageView sizeToFit];
+    self.imageView.frame = CGRectMake(0, 0, image.size.width, image.size.height);
+    
+    // self.scrollView could be nil on the next line if outlet-setting has not happened yet
     self.scrollView.contentSize = self.image ? self.image.size : CGSizeZero;
     [self.pizza stopAnimating];
 }
